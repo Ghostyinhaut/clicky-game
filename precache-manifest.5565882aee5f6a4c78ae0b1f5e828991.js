0e3a26d673c0ad060ac32117d12e63a6c8b9ed8a
Michael Hu <upsidedownhu@gmail.com>
2020-01-22 20:17:48 -0800

    Updates

---
+++ precache-manifest.5565882aee5f6a4c78ae0b1f5e828991.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715ad432ac6be98bc6481fbb2b1d3bff",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "dc862fdb20ed80c580d2",
+    "url": "/clicky-game/static/css/main.66b5ac94.chunk.css"
+  },
+  {
+    "revision": "503d89b3b7ff06bf56e0",
+    "url": "/clicky-game/static/js/2.1cce1e1c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.1cce1e1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc862fdb20ed80c580d2",
+    "url": "/clicky-game/static/js/main.754c16bf.chunk.js"
+  },
+  {
+    "revision": "d1e0a912f91efe35ebbe",
+    "url": "/clicky-game/static/js/runtime-main.ba0aa96a.js"
+  }
+]);
